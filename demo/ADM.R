3013e0e94ab816f2d84ca36020827579c8915471
daniel.bonnery@gmail.com <daniel.bonnery@gmail.com>
2015-07-20 15:38:33 -0400

    a

--- demo/ADM.R
+++ demo/ADM.R
@@ -13,7 +13,6 @@ summary(g)
 plot(g)
 #3.3 Posterior densities 
 #Computation of the posterior distribution of A parameters
-sapply(c("Rgbp","abind","surv798B","plyr","lattice","TANOVA"),require, character.only=T)
 #we load this library to get trigammaInverse and digamma functions 
 library("TANOVA")
 alpha=trigammaInverse(g$a.var)
@@ -25,14 +24,13 @@ A.summary<-c(Mean=A.mean,sd=A.sd, qgamma(c(.5,.025,.975),shape=alpha,rate=beta))
 names(A.summary)[3:5]<-c("Median","1st Qu.","3rd Qu.")
 #Definition of the x sequences for the plots 
 x=seq(-1,1,length.out=200)
-x1<-g$beta.new[2,1]+x*3*sqrt(g$beta.var[2,2])
-x2<-seq(qgamma(.001,alpha,rate=beta),qgamma(.999,alpha,rate=beta),length.out=200)
-x3<-g$post.mean[1,1]+x*3*g$post.sd[1,1]
+X1<-g$beta.new[2,1]+x*3*sqrt(g$beta.var[2,2])
+X2<-seq(qgamma(.001,alpha,rate=beta),qgamma(.999,alpha,rate=beta),length.out=200)
+X3<-g$post.mean[1,1]+x*3*g$post.sd[1,1]
 #plot posterior densityes
 par(mfrow=c(3,1))
-plot(x1,dnorm(x1,mean=g$beta.new[2,1],sd=sqrt(g$beta.var[2,2])),xlab='Posterior density of beta1',ylab='density',type='l',col=couleurdebase,lwd=3)
-plot(x2,dgamma(x2,shape=alpha,rate=beta),xlab='Posterior density of A',ylab='density',type='l',col=couleurdebase,lwd=3)
+plot(X1,dnorm(x1,mean=g$beta.new[2,1],sd=sqrt(g$beta.var[2,2])),xlab='Posterior density of beta1',ylab='density',type='l',col="black",lwd=3)
+plot(X2,dgamma(x2,shape=alpha,rate=beta),xlab='Posterior density of A',ylab='density',type='l',col="black",lwd=3)
 abline(v=A.mean,lwd=3,col='gray')
 abline(v=A.summary['Median'],lwd=3,col='darkgray')
-par(mfrow=c(2,1))
-plot(x3,dnorm(x3,mean=g$post.mean[1,1],sd=g$post.sd[1,1]),xlab='Posterior density of $theta_i$ for Clemente.',ylab='density',type='l',col=couleurdebase,lwd=3)
+plot(X3,dnorm(x3,mean=g$post.mean[1,1],sd=g$post.sd[1,1]),xlab='Posterior density of $theta_i$ for Clemente.',ylab='density',type='l',col="black",lwd=3)
