c969b6c1099fea9c5414bb94ccbb90dc7209f985
daniel.bonnery@gmail.com <daniel.bonnery@gmail.com>
2015-07-20 22:26:08 -0400

    a

--- demo/EB2.R
+++ demo/EB2.R
@@ -1,5 +1,4 @@
-data(baseball)
-attach(baseball)
+data(baseball);attach(baseball)
 library("sae")
-resultML<-mseFH(hat.P~x1,hat.P*(1-hat.P)/45,method="ML")
+resultML<-mseFH(hat.P~x1,mean(hat.P)*(1-mean(hat.P))/45,method="ML")
 resultML
